b7a2ec6ae1b8ddae4b3184dcae55ce24875678df
amitrajgodara <65665093+amitrajgodara@users.noreply.github.com>
2020-10-13 20:56:16 +0530

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,13 @@
+class Form {
+    constructor(){}
+ display(){
+     var title=createElement("h2");
+     title.html("car racing game");
+     title.position(130,0);
+
+     var input=createInput("name");
+     input.position(130,160);
+     var button=createButton("play");
+     button.position(250,200);
+ }
+}
